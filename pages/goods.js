547ef17c4da14219e37698128946bf648e33f840
maeve <maeve.oh@flowerfarm.co.kr>
2022-09-19 18:58:10 +0900

    commit

--- pages/goods.js
+++ pages/goods.js
@@ -18,29 +18,30 @@ function goods({ data }) {
 					/>
 				</div>
 				<div className='summary-wrap'>
-					<div className=''>
+					<div className='' style={{fontSize: 20}}>
 						{data[0].productName}
 					</div>
 					<div className='line'>
-						{price}원
+						<div style={{fontSize: 24, fontWeight: 700}}>{price}</div>
+						<div style={{fontSize: 16}}>원</div>
 					</div>
 					<div className='line-container'>
 						<dl className='line'>
 							<dt>배송</dt>
-							<dd></dd>
+							<dd>당일배송</dd>
 						</dl>
 						<dl className='line'>
 							<dt>판매자</dt>
-							<dd></dd>
+							<dd>꽃팜</dd>
 						</dl>
 						<dl className='line'>
 							<dt>판매단위</dt>
-							<dd></dd>
+							<dd>상품명 참조</dd>
 						</dl>
 					</div>
 					<div className=''>
 						<dl className='line'>
-							<dt style={{flex: 1}}>구매수량</dt>
+							<dt>구매수량</dt>
 							<div className='count'>
 								<Button size='small'>-</Button>
 								<div style={{width: 60, height: 24, textAlign:'center'}}>1</div>
@@ -58,6 +59,8 @@ function goods({ data }) {
 			article { display: flex; justify-content: space-between; }
 			.summary-wrap { width: 400px; }
 			.line { border-bottom: 1px solid #aaa; display: flex; padding-bottom: 14px; }
+			dt { flex: 1; }
+			dd { flex: 2; }
 			.count { flex: 2; display: flex; }
 			`}</style>
 		</ContentWrap>
